58c4cad21cbe564f3ccba5a200d75b1db7cc6a37
Brian Malloy <malloy@clemson.edu>
2018-11-14 16:34:08 -0500

    new changes

---
+++ projects/3/infinite/world.h
@@ -0,0 +1,20 @@
+#include <string>
+#include "image.h"
+#include "viewport.h"
+
+class World {
+public:
+  World(const std::string& name, int fact);
+  // ~World() { } // The image will be deleted by the FrameFactory
+  void update();
+  void draw() const;
+private:
+  Image* const image;
+  int factor;
+  unsigned imageWidth;
+  float viewX;
+  float viewY;
+  const Viewport & view;
+  World(const World&);
+  World& operator=(const World&);
+};
