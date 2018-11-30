a91effa6c749e6edc1dbbd30d625b34f40929e6a
Brian Malloy <malloy@clemson.edu>
2018-11-30 15:24:24 -0500

    new changes

---
+++ examples/sdl/explosions/fixed/engine.h
@@ -0,0 +1,36 @@
+#include <vector>
+#include <SDL.h>
+#include "ioMod.h"
+#include "renderContext.h"
+#include "clock.h"
+#include "world.h"
+#include "viewport.h"
+
+class Engine {
+public:
+  Engine ();
+  ~Engine ();
+  void play();
+  void switchSprite();
+
+private:
+  const RenderContext* rc;
+  const IOmod& io;
+  Clock& clock;
+
+  SDL_Renderer * const renderer;
+  World world;
+  Viewport& viewport;
+
+  std::vector<Drawable*> sprites;
+
+  int currentSprite;
+  bool makeVideo;
+
+  void draw() const;
+  void update(Uint32);
+
+  Engine(const Engine&);
+  Engine& operator=(const Engine&);
+  void printScales() const;
+};
---
+++ examples/sdl/explosions/frames/engine.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <random>
+#include <iomanip>
+#include "sprite.h"
+#include "multisprite.h"
+#include "gamedata.h"
+#include "engine.h"
+#include "frameGenerator.h"
+
+Engine::~Engine() { 
+  for ( Drawable* sprite : sprites ) {
+    delete sprite;
+  }
+  std::cout << "Terminating program" << std::endl;
+}
+
+Engine::Engine() :
+  rc( RenderContext::getInstance() ),
+  io( IOmod::getInstance() ),
+  clock( Clock::getInstance() ),
+  renderer( rc->getRenderer() ),
+  world("back", Gamedata::getInstance().getXmlInt("back/factor") ),
+  viewport( Viewport::getInstance() ),
+  sprites(),
+  currentSprite(0),
+  makeVideo( false )
+{
+  sprites.push_back( new MultiSprite("explode2") );
+  sprites.push_back( new MultiSprite("explode1") );
+  Viewport::getInstance().setObjectToTrack(sprites[0]);
+  std::cout << "Loading complete" << std::endl;
+}
+
+void Engine::draw() const {
+  world.draw();
+
+  sprites[currentSprite]->draw();
+
+  viewport.draw();
+  SDL_RenderPresent(renderer);
+}
+
+void Engine::update(Uint32 ticks) {
+  sprites[currentSprite]->update(ticks);
+  world.update();
+  viewport.update(); // always update viewport last
+}
+
+void Engine::switchSprite(){
+  ++currentSprite;
+  currentSprite = currentSprite % 2;
+  Viewport::getInstance().setObjectToTrack(sprites[currentSprite]);
+}
+
+void Engine::play() {
+  SDL_Event event;
+  const Uint8* keystate;
+  bool done = false;
+  Uint32 ticks = clock.getElapsedTicks();
+  FrameGenerator frameGen;
+
+  while ( !done ) {
+    // The next loop polls for events, guarding against key bounce:
+    while ( SDL_PollEvent(&event) ) {
+      keystate = SDL_GetKeyboardState(NULL);
+      if (event.type ==  SDL_QUIT) { done = true; break; }
+      if(event.type == SDL_KEYDOWN) {
+        if (keystate[SDL_SCANCODE_ESCAPE] || keystate[SDL_SCANCODE_Q]) {
+          done = true;
+          break;
+        }
+        if ( keystate[SDL_SCANCODE_P] ) {
+          if ( clock.isPaused() ) clock.unpause();
+          else clock.pause();
+        }
+        if ( keystate[SDL_SCANCODE_T] ) {
+          switchSprite();
+        }
+        if (keystate[SDL_SCANCODE_F4] && !makeVideo) {
+          std::cout << "Initiating frame capture" << std::endl;
+          makeVideo = true;
+        }
+        else if (keystate[SDL_SCANCODE_F4] && makeVideo) {
+          std::cout << "Terminating frame capture" << std::endl;
+          makeVideo = false;
+        }
+      }
+    }
+
+    // In this section of the event loop we allow key bounce:
+
+    ticks = clock.getElapsedTicks();
+    if ( ticks > 0 ) {
+      clock.incrFrame();
+      draw();
+      update(ticks);
+      if ( makeVideo ) {
+        frameGen.makeFrame();
+      }
+    }
+  }
+}
